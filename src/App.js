3756fe4cbcb3753363ba8953e316a0d25d43e188
Evgeniya3010 <susskaya86@gmail.com>
2021-02-13 05:48:28 +0200

    basic features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import UserProfile from "./components/UserProfile";
+import user from "./user.json";
+
+const user1 = user[1];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UserProfile
+        name={user1.name}
+        tag={user1.tag}
+        location={user1.location}
+        avatar={user1.avatar}
+        followers={user1.stats.followers}
+        views={user1.stats.views}
+        likes={user1.stats.likes}
+      />
+    </div>
+  );
 }
 
 export default App;
